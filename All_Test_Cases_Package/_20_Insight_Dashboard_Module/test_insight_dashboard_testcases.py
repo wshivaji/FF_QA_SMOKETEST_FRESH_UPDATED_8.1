7d1e382dc65cf078f49b960dc0942a44eaf3ae41
Facefirst-repo <shviaji.wankhede@facefirst.com>
2024-04-15 12:16:12 +0530

    Insight Dashboard updated.

---
+++ All_Test_Cases_Package/_20_Insight_Dashboard_Module/test_insight_dashboard_testcases.py
@@ -0,0 +1,195 @@
+import pytest
+from Base_Package.Web_Driver import web_driver
+from Base_Package.Web_Logger import web_logger
+from All_POM_Packages._20_Insight_Dashboard_Module_POM.Insight_Dashboard_POM import insight_dashboard_pom
+
+
+@pytest.mark.run(order=21)
+class Test_insight_dashboard_test_cases(web_driver, web_logger):
+    logger = web_logger.logger_obj()
+    logger.info(" ******** Insight Dashboard (Order - 21) Begin ********")
+    print("******** Insight Dashboard (Order - 21) Begin ********")
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_01(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_counts_on_overview_dashboard_as_total_loss_prevented_5500_active_enrollment_25_total_match_events_25_visito_searches_0_investgation_saving_time_0_repeat_people_of_interest_0():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_02(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_loss_prevented_by_enrollment_group_counts():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_03(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_Possible_Match_Events_by_enrollment_action_counts():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_04(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_counts_on_overview_dashboard_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_05(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_loss_prevented_by_enrollment_group_counts_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_06(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_Possible_Match_Events_by_enrollment_action_counts_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_07(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_counts_on_Probable_Match_Events_Dashboard():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_08(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_cumulative_Probable_Match_Events_by_date_todays_date_and_25_count():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_09(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_tagged_vs_untagged_Probable_Match_Events_count_as_tagged_and_untagged():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_10(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_Probable_Match_Events_by_enrollment_groups_counts():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_11(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_tagged_Probable_Match_Events_by_tag_type():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_12(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_counts_on_Probable_Match_Events_dashboard_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_13(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_cumulative_Probable_Match_Events_by_date_organisation_and_individual_group():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_14(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_tagged_vs_untagged_Probable_Match_Events_count_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_15(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_Probable_Match_Events_by_enrollment_groups_counts_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_16(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_tagged_Probable_Match_Events_by_tag_type_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_17(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_on_enrollment_dashboard_as_enrollment_overview_and_cumulative_enrollments_by_date_as_mention_is_link():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_18(self):
+        if insight_dashboard_pom().Verify_enrollment_by_date_counts_on_enrollment_dashboard():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_19(self):
+        if insight_dashboard_pom().Verify_enrollments_by_enrollment_group_counts_as_mention_in_link():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_20(self):
+        if insight_dashboard_pom().Verify_enrollments_by_week_org_counts_as_mention_in_link():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_21(self):
+        if insight_dashboard_pom().Verify_Enrollments_by_status_count():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_22(self):
+        if insight_dashboard_pom().Verify_user_is_able_to_see_on_enrollment_dashboard_as_enrollment_overview_and_cumulative_enrollments_by_date():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_23(self):
+        if insight_dashboard_pom().Verify_enrollment_by_date_counts_as_mention_in_link_organisation_and_individual_group():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_24(self):
+        if insight_dashboard_pom().Verify_enrollments_by_enrollment_group_counts_as_mention_in_link_organisation_and_individual_group():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_25(self):
+        if insight_dashboard_pom().Verify_enrollments_by_week_counts_as_mention_in_link_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    @pytest.mark.p1
+    def test_TC_Insight_Dashboard_26(self):
+        if insight_dashboard_pom().Verify_Enrollments_by_Status_counts_organisation_and_individual_groups():
+            assert True
+        else:
+            assert False
+
+    # ********************************************* P1 Completed ****************************************************
--- Base_Package/Login_Logout_Ops.py
+++ Base_Package/Login_Logout_Ops.py
@@ -354,6 +354,79 @@ class login(web_driver, web_logger):
             self.d.save_screenshot(f"{web_driver.screenshots_path}\\login_exception.png")
             print(f"{ex.args}")
 
+    def login_to_facecirst_portal_if_not_done(self, d):
+        try:
+            self.d = d
+            time.sleep(web_driver.one_second)
+            # print(f"current url: {self.d.current_url}")
+            # print(f"expected url: {Portal_login_page_read_ini().get_portal_url()}")
+            logout_btn = cloud_menu_btn = None
+            try:
+                # self.d.refresh()
+                logout_btn = web_driver.explicit_wait(self, 3, "XPATH",
+                                                      self.config.get("logout_locators", "logout_btn_by_xpath"), self.d)
+                cloud_menu_btn = web_driver.explicit_wait(self, 5, "XPATH", self.config.get("logout_locators",
+                                                                                            "cloud_or_local_menu_by_xpath"),
+                                                          self.d)
+            except Exception as ex:
+                print(ex.args)
+            if cloud_menu_btn:
+                print(f"logout btn visible: {cloud_menu_btn.is_displayed()}")
+            else:
+                if self.d.current_url == Portal_login_page_read_ini().get_portal_url():
+                    print(f"page url: {self.d.current_url}")
+                else:
+                    self.d.get(Portal_login_page_read_ini().get_portal_url())
+                    self.d.maximize_window()
+                    time.sleep(web_driver.two_second)
+                    # self.d.find_element(By.XPATH, Portal_login_page_read_ini().get_advance_btn_by_xpath()).click()
+                    # self.logger.info("clicked on advance")
+                    # time.sleep(web_driver.one_second)
+                    # self.logger.info(f"proceed link xpath: {Portal_login_page_read_ini().get_proceed_link_by_xpath()}")
+                    # self.d.find_element(By.XPATH, Portal_login_page_read_ini().get_proceed_link_by_xpath()).click()
+                    # self.logger.info("clicked on proceed link")
+                    # time.sleep(web_driver.one_second)
+                    time.sleep(web_driver.one_second)
+                    for i in range(4):
+                        pyautogui.hotkey('ctrl', '-')
+                        time.sleep(0.5)
+                    username_textbox = web_driver.explicit_wait(self, 10, "XPATH",
+                                                                self.config.get("login_page_locators",
+                                                                                "username_textbox_by_xpath"),
+                                                                self.d)
+                    password_textbox = self.d.find_element(By.XPATH,
+                                                           self.config.get("login_page_locators",
+                                                                           "password_textbox_by_xpath"))
+                    login_btn = self.d.find_element(By.XPATH,
+                                                    self.config.get("login_page_locators", "login_link_by_xpath"))
+                    self.implicit_wait(web_driver.one_second, self.d)
+                    if username_textbox.text == "":
+                        username_textbox.send_keys(self.config.get("user_info", "username"))
+                    self.implicit_wait(web_driver.one_second, self.d)
+                    if password_textbox.text == "":
+                        password_textbox.send_keys(self.config.get("user_info", "password"))
+                    self.implicit_wait(web_driver.one_second, self.d)
+                    login_btn.click()
+                    self.implicit_wait(web_driver.one_second, self.d)
+                    # logout_btn = self.d.find_element(By.XPATH, self.config.get("logout_locators", "logout_btn_by_xpath"))
+                    logout_btn = web_driver.explicit_wait(self, 10, "XPATH",
+                                                          self.config.get("logout_locators", "logout_btn_by_xpath"),
+                                                          self.d)
+                    time.sleep(web_driver.one_second)
+                    if logout_btn.is_displayed():
+                        self.logger.info("login success..")
+                    else:
+                        self.logger.info("login unsuccessful.. please check code.")
+                        self.d.save_screenshot(f"{web_driver.screenshots_path}\\login_failed.png")
+                    time.sleep(web_driver.two_second)
+
+            return self.d
+        except Exception as ex:
+            self.logger.info(f"exception: {ex.args}")
+            self.logger.info(f"exception message: {ex}")
+            self.d.save_screenshot(f"{web_driver.screenshots_path}\\login_exception.png")
+            print(f"{ex.args}")
+
 class logout(web_driver, web_logger):
     def __init__(self):
         self.file_path = f"{Path(__file__).parent.parent}\\All_Test_Data\\0_Login_Logout_Data\\Data_From_INI\\login_logout.ini"
