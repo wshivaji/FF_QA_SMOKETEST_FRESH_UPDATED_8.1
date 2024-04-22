6544e72b5df2d0a29f67b5df8daf169b9debfc0b
Facefirst-repo <shviaji.wankhede@facefirst.com>
2024-04-22 19:16:24 +0530

    tests updated

--- All_Test_Cases_Package/_22_Zones_Module/test_Zones_testcases.py
+++ All_Test_Cases_Package/_22_Zones_Module/test_Zones_testcases.py
@@ -12,24 +12,28 @@ class Test_Zones_Module_Test_Cases(web_driver, web_logger):
     logger.info(" ******** ZONES_Module (Order - 22) Begin ********")
     print("******** ZONES_Module (Order - 22) Begin ********")
 
+    @pytest.mark.system
     def test_Zones_TC_001(self):
         if Zones_pom().open_zones_panel_and_verify_zones_panel_displayed():
             assert True
         else:
             assert False
 
+    @pytest.mark.system
     def test_zones_TC_002(self):
         if Zones_pom().verify_zone_list_enlisted_and_zone_names_displayed_as_expected():
             assert True
         else:
             assert False
 
+    @pytest.mark.system
     def test_zones_TC_003(self):
         if Zones_pom().verify_zone_list_enlisted_and_zone_details_btn_displayed_as_expected():
             assert True
         else:
             assert False
 
+    @pytest.mark.system
     def test_zones_TC_004(self):
         if Zones_pom().verify_zone_list_enlisted_and_zone_select_checkbox_displayed_as_expected():
             assert True
